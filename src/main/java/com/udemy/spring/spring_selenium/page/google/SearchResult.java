c6671d958650ac6392d7c64c61b2ac265c808806
namratamishra <namratta10@gmail.com>
2025-07-15 10:09:51 +0530

    searchResult

--- src/main/java/com/udemy/spring/spring_selenium/page/google/SearchResult.java
+++ src/main/java/com/udemy/spring/spring_selenium/page/google/SearchResult.java
@@ -1,4 +1,11 @@
 package com.udemy.spring.spring_selenium.page.google;
 
-public class SearchResult {
+import com.udemy.spring.spring_selenium.page.Base;
+
+public class SearchResult extends Base {
+
+    @Override
+    public boolean isAt() {
+        return false;
+    }
 }
